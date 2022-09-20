629cdc6069ae80548f975ac1a09ad02279df9f51
jemin98 <jeminsosaliya@gmail.com>
2022-09-20 07:40:25 +0530

    initial code

---
+++ android/app/src/main/kotlin/com/example/provider_project_structure/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provider_project_structure
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utilities/asset/asset_utilities.dart
@@ -0,0 +1,7 @@
+/// all the assets, images, video, audio and other files
+/// used in the application.
+///
+/// are defined in this file.
+class AssetUtilities {
+  static const String _baseUrl = 'assets/';
+}
